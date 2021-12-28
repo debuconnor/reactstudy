528e841edcdc40da0a8303401d4a8fee92cdb4f3
debuconnor <94517537+debuconnor@users.noreply.github.com>
2021-12-28 21:01:30 +0900

    variable daya types

--- javascript/variable.js
+++ javascript/variable.js
@@ -3,9 +3,17 @@ function scope(){
    let b = 0;
    var c = 0;
 
+   console.log("init: ", a, b, c);
+
    if(a===0){
       const a = 1;
       let b = 1;
       var c = 1;
+
+      console.log("if block: ", a, b, c)
    }
+
+   console.log("function block: ", a, b, c);
 }
+
+scope()
