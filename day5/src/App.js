68ff6347b84a14b8617ece955bc5e86248b7a9d9
debuconnor <94517537+debuconnor@users.noreply.github.com>
2022-01-06 21:20:46 +0900

    practicing useState

--- day5/src/App.js
+++ day5/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
-import React from 'react'
-import InputRef from './InputRef';
+import React, { useState } from 'react'
 
 function App() {
+  const [text, setText] = useState('')
 
-  return(
+  const onChange = (e) => {
+    setText(e.target.value);
+  }
+
+  const clearText = (e) =>{
+    setText('')
+  }
+
+  return (
     <div className='App'>
-      <InputRef />
+      <input onChange={onChange} value={text} />
+      <button onClick={clearText}>clear</button>
+      <div>val: {text}</div>
     </div>
   )
 }
