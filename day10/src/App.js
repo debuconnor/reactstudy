c5c928b835a7d4dfce9641e4c216ea8fa99fe1fa
debuconnor <gterppw@gmail.com>
2022-01-20 20:00:51 +0900

    not found page using switch clause

--- day10/src/App.js
+++ day10/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Route, } from 'react-router-dom'
+import { Route, Switch, } from 'react-router-dom'
 import Graphics from './Graphics'
 import Home from './Home'
+import NotFound from './NotFound'
 import Web from './Web'
 
 function App() {
@@ -9,16 +10,20 @@ function App() {
     <div className='App'>
       <h1>React Router Dom</h1>
       <hr />
-      <Route path='/' exact>
-        <Home />
-      </Route>
-      <Route path='/web'>
-        <Web />
-      </Route>
-      <Route path='/graphic'>
-        <Graphics />
-      </Route>
-
+      <Switch>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <Route path='/web'>
+          <Web />
+        </Route>
+        <Route path='/graphic'>
+          <Graphics />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
     </div>
   )
 }
